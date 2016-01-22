32b99b155ded84e6b1cd7cf0663fcb2a12e4cd0e
neeharikasubbu <neeharikasubbu>
2016-01-21 20:46:57 -0500

    after commting Array2

---
+++ Array2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Array2{
+public static void main(String[] args){
+
+Scanner scan = new Scanner(System.in);
+
+int[] a = new int[4];
+
+
+for(int i=0;i<=3;i++){
+System.out.println(a[i]);
+a[i] = scan.nextInt();
+}
+for(int i=0;i<=a.length;i++){
+System.out.println(a[i]);
+}
+}
+}
+
+//here we are storing the values and again taking them in side
